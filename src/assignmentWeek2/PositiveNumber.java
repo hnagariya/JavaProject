0b3fc5b64980955b270088e8732a98558496f4b0
hnagariya <neelam.nagariya@gmail.com>
2023-04-02 15:56:17 -0400

    assignment week 2

--- src/assignmentWeek2/PositiveNumber.java
+++ src/assignmentWeek2/PositiveNumber.java
@@ -9,24 +9,22 @@ public class PositiveNumber {
 	void isPositiveNumberPrime() {
 		System.out.println("Enter positive number");
 		n = sc.nextInt();
+		boolean notPrime=false;
 		if (n >= 0) {
-			if (n >= 2) {
-
-				for (int i = 2; i < n; i++) {
-					int r = n % i;
-					if (r == 0) {
-						System.out.println("Entered positive number is not a Prime number");
-					}
+			for (int i = 2; i < n; i++) {
+				int r = n % i;
+				if (r == 0) {
+					notPrime = true;
 					break;
-
 				}
-			} else {
-				System.out.println("Entered number is a prime number");
 			}
-		}
-
-		else {
+		} else {
 			System.out.println("It is not a positive number. Please enter a positive number");
+		}
+		if (notPrime) {
+			System.out.println("Entered positive number is not a Prime number");
+		} else {
+			System.out.println("It is prime.");
 		}
 	}
 }
